ada21dd2ba38c047bd5d5bb81cd3dca954a03103
Dovydas Dosinas <dovydas.dosinas@knf.stud.vu.lt>
2022-09-26 14:28:43 +0300

    Initial commit

---
+++ app/src/main/java/com/example/mipt_1_praktikos_darbas_ide_setup/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.mipt_1_praktikos_darbas_ide_setup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
