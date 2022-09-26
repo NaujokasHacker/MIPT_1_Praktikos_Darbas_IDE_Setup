b497f6426d949f466b95bbbb2d7b63a6fa6d7ba8
Dovydas Dosinas <dovydas.dosinas@knf.stud.vu.lt>
2022-09-26 15:38:47 +0300

    Added change text button

--- app/src/main/java/com/example/mipt_1_praktikos_darbas_ide_setup/MainActivity.java
+++ app/src/main/java/com/example/mipt_1_praktikos_darbas_ide_setup/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.mipt_1_praktikos_darbas_ide_setup;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView twMain;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        twMain = (TextView)findViewById(R.id.twMain);
+
+    }
+    public void onBtnChangeTextClick(View view){
+                twMain.setText("Hello New");
     }
 }
